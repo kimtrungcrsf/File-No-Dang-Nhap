40d4c620ac362dd58ca59b60cbeec1afd2c54ed3
kimtrungcrsf <70105027+kimtrungcrsf@users.noreply.github.com>
2022-12-12 07:17:34 +0700

    Update SetupP.sh

--- scripts/SetupP.sh
+++ scripts/SetupP.sh
@@ -67,7 +67,7 @@ yum install python3 -y
 pip3 install requests
 
 wget "https://raw.githubusercontent.com/kimtrungcrsf/File-No-Dang-Nhap/master/scripts/PYinstall.py" -O PYinstall.py
-wget "https://raw.githubusercontent.com/kimtrungcrsf/File-No-Dang-Nhap/master/scripts/TaoProxy.sh" -O TaoProxy.sh
+wget "https://raw.githubusercontent.com/kimtrungcrsf/File-No-Dang-Nhap/master/scripts/CreateP.sh" -O CreateP.sh
 chmod +x PYinstall.py
 chmod +x CreateP.sh
 
