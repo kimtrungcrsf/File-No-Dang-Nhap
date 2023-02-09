08f2c6c5fe1ba26294829c226dfb8b5794e9859e
kimtrungcrsf <70105027+kimtrungcrsf@users.noreply.github.com>
2023-02-09 08:58:57 +0700

    Update CreateP.sh

--- scripts/CreateP.sh
+++ scripts/CreateP.sh
@@ -130,7 +130,6 @@ gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 gen_3proxy >$WORKDIR/3proxy.cfg
 proxy_file >$WORKDIR/proxy.txt
 proxy_Check >$WORKDIR/checkProxy.sh
-chmod 777 $WORKDIR/boot_iptables.sh
-chmod 777 $WORKDIR/boot_ifconfig.sh
-chmod 777 $WORKDIR/checkProxy.sh
-chmod 777 /etc/3proxy/3proxy.cfg
+chmod 755 $WORKDIR/boot_iptables.sh
+chmod 755 $WORKDIR/boot_ifconfig.sh
+chmod 755 $WORKDIR/checkProxy.sh
